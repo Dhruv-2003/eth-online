e9556c5d12d3ecb8d91f623a1a1add06c440ef97
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2023-10-20 11:23:06 +0530

    ui: basic login page

--- frontend/pages/get-started.tsx
+++ frontend/pages/get-started.tsx
@@ -1,9 +1,12 @@
+import { CreateAccount } from "@/components/auth/auth-form";
 import React from "react";
 
 export default function Onboarding() {
   return (
-    <div>
-      <h1>Onboarding</h1>
+    <div className=" flex items-center justify-center min-h-screen">
+      <div className=" h-full w-1/3">
+        <CreateAccount />
+      </div>
     </div>
   );
 }
