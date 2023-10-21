a25889d563c182cf900b67607657690837b5ed36
Architsharma7 <architsharma7911@gmail.com>
2023-10-22 01:27:36 +0530

    fix: notifi node methods added

--- frontend/pages/campaign.tsx
+++ frontend/pages/campaign.tsx
@@ -1,10 +1,13 @@
+import Notifi from "@/components/notifi";
 import DashboardNavigation from "@/components/ui/sidebar";
 import React from "react";
 
 export default function Campaign() {
   return (
     <DashboardNavigation>
-      <div>Campaign</div>
+      <div>
+        <Notifi/>
+      </div>
     </DashboardNavigation>
   );
 }
