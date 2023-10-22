2ba4f7f962bb58ae60cd9515011df529413ee7f9
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-10-22 17:07:35 +0530

    feat: Invite user modal functions compelte

--- frontend/components/invite-modal.tsx
+++ frontend/components/invite-modal.tsx
@@ -15,9 +15,80 @@ import Image from "next/image";
 import success from "@/assets/popper.gif";
 import check from "@/assets/check.gif";
 import { useState } from "react";
+import { computePublicKey } from "@/utils/Lit";
+import { createSafeWallet } from "@/utils/Safe";
+import { addUser } from "./firebase/methods";
+import { sendEmail } from "@/utils/mail";
+
+const STYTCH_PROJECT_ID: string | undefined =
+  process.env.NEXT_PUBLIC_STYTCH_PROJECT_ID;
+const DISCORD_CLIENT_ID: string | undefined =
+  process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID;
 
 export function InviteFriend({ modalTrigger }: { modalTrigger?: JSX.Element }) {
   const [inviteSuccess, setInviteSuccess] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>();
+  const [mobile, setMobile] = useState<string>();
+  const [name, setName] = useState<string>();
+
+  const completeInvite = async () => {
+    try {
+      // take the inputs
+      // calculate the user address
+      const data = await getPubKey();
+      if (!data) {
+        console.log("Pub Key can't be calculate");
+        return;
+      }
+      // create safeFortheUser
+      const safeSDK = await createSafeWallet(data?.address, "9");
+      // safe SDK instance is provider & safeAddress
+      const safeAddress = await safeSDK?.getAddress();
+
+      // store data on firebase , add(name , email , pkpAddress, safeAddress)
+      // addUser();
+
+      // initiate an email invite  , with inviting user to the platform
+      // sendEmail()
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getPubKey = async (): Promise<
+    { address: string; pubKey: string } | undefined
+  > => {
+    try {
+      if (email) {
+        const res = await fetch("/api/getUser", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ mobile, email }),
+        });
+        const result = await res.json();
+        const user_id = result.user[0].user_id;
+        console.log(user_id);
+
+        if (!STYTCH_PROJECT_ID) {
+          throw Error(
+            "Could not find stytch project secret or id in enviorment"
+          );
+        }
+
+        // compute public Key
+        //   console.log(user_id, STYTCH_PROJECT_ID);
+        const data = await computePublicKey(user_id, STYTCH_PROJECT_ID);
+        console.log(data);
+        return data;
+      } else {
+        console.log("No Input found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <Dialog>
@@ -39,7 +110,14 @@ export function InviteFriend({ modalTrigger }: { modalTrigger?: JSX.Element }) {
                 <Label htmlFor="name" className="text-right">
                   Friend&#39;s Name
                 </Label>
-                <Input id="name" placeholder="Alice" className="col-span-3" />
+                <Input
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                  id="name"
+                  placeholder="Alice"
+                  className="col-span-3"
+                />
               </div>
               <div className="grid grid-cols-4 items-center gap-4">
                 <Label htmlFor="username" className="text-right">
@@ -48,13 +126,21 @@ export function InviteFriend({ modalTrigger }: { modalTrigger?: JSX.Element }) {
                 <Input
                   id="email"
                   type="email"
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
                   placeholder="friend@example.com"
                   className="col-span-3"
                 />
               </div>
             </div>
             <DialogFooter>
-              <Button type="submit">Send Invitataion</Button>
+              <Button type="submit" onClick={getPubKey}>
+                Get User address
+              </Button>
+              <Button type="submit" onClick={completeInvite}>
+                Send Invitataion
+              </Button>
             </DialogFooter>
           </>
         ) : (
